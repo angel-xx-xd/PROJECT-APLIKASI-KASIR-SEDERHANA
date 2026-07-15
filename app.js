67b2ac597d9852536ea4dc9c1b1216f5d1c19aaf
angel <angelazagoto16@gmail.com>
2026-07-15 03:56:53 +0000

    Update project

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const { engine } = require("express-handlebars");
 const path = require("path");
 const session = require("express-session");
 
+const { isAuthenticated } = require("./middlewares/authMiddleware");
+
+const DashboardController = require("./controllers/DashboardController");
+
 const authRoutes = require("./routes/auth");
 const categoryRoutes = require("./routes/category");
 const productRoutes = require("./routes/product");
@@ -17,12 +21,14 @@ const app = express();
 // ======================
 // Body Parser
 // ======================
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // ======================
 // Session
 // ======================
+
 app.use(
     session({
         secret: process.env.SESSION_SECRET || "kasir-secret",
@@ -37,6 +43,7 @@ app.use(
 // ======================
 // Handlebars
 // ======================
+
 app.engine(
     "hbs",
     engine({
@@ -52,6 +59,7 @@ app.set("views", path.join(__dirname, "views"));
 // ======================
 // Static Files
 // ======================
+
 app.use(
     "/bootstrap",
     express.static(path.join(__dirname, "node_modules/bootstrap/dist"))
@@ -62,32 +70,26 @@ app.use(express.static(path.join(__dirname, "public")));
 // ======================
 // Session ke View
 // ======================
+
 app.use((req, res, next) => {
-
     res.locals.session = req.session;
-
     next();
-
 });
 
 // ======================
 // Dashboard
 // ======================
-app.get("/", (req, res) => {
 
-    if (!req.session.user_id) {
-        return res.redirect("/auth/login");
-    }
-
-    res.render("pages/dashboard/index", {
-        title: "Dashboard"
-    });
-
-});
+app.get(
+    "/",
+    isAuthenticated,
+    DashboardController.index
+);
 
 // ======================
 // Routes
 // ======================
+
 app.use("/auth", authRoutes);
 app.use("/kategori", categoryRoutes);
 app.use("/produk", productRoutes);
@@ -97,30 +99,26 @@ app.use("/pembayaran", paymentRoutes);
 // ======================
 // 404
 // ======================
+
 app.use((req, res) => {
-
     res.status(404).send("404 | Halaman tidak ditemukan");
-
 });
 
 // ======================
 // Error Handler
 // ======================
+
 app.use((err, req, res, next) => {
-
     console.error(err);
-
     res.status(500).send("Terjadi kesalahan pada server.");
-
 });
 
 // ======================
 // Start Server
 // ======================
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-
     console.log(`Server berjalan di http://localhost:${PORT}`);
-
 });
